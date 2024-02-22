9607ee44b4953f96ae612f6378230b78a693aeff
Dung Nguyen <contact@dungnt.net>
2024-02-22 21:47:16 +0700

    update xo/usql v0.17.5

--- Casks/usql.rb
+++ Casks/usql.rb
@@ -1,9 +1,9 @@
 cask "usql" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.17.0"
-  sha256  arm:   "67021ad7942fcce8eac4e3efce449e5223a60ad242b4e9801805ec0bfd4c2e84",
-          intel: "637c9be294aed69aa7f2451219cad4e9ce2bfe0148042ad0c80d90d3715a3212"
+  version "0.17.5"
+  sha256  arm:   "08d1f56a393eeca6c34919e170beb6abe184ac07ec9ec37c344bfa683e0a04c7",
+          intel: "1fabe95ea1b4d608579026643d29d12e05eee0e0c97a7e9ac57628f5889842cb"
 
   url "https://github.com/xo/usql/releases/download/v#{version}/usql-#{version}-darwin-#{arch}.tar.bz2"
   name "xo/usql"
