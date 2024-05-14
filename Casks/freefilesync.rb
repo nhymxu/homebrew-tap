0e3ce49414720298782c34c21729b83a5a6a78d3
Dung Nguyen <contact@dungnt.net>
2024-05-14 16:32:25 +0700

    add FreeFileSync

---
+++ Casks/freefilesync.rb
@@ -0,0 +1,15 @@
+cask 'freefilesync' do
+  version '13.6'
+  sha256 :no_check # required as upstream package is updated in-place
+
+  url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip"
+  name 'FreeFileSync'
+  homepage 'https://www.freefilesync.org/'
+
+  pkg "FreeFileSync_#{version}.pkg"
+
+  uninstall pkgutil: [
+    'org.freefilesync.pkg.FreeFileSync',
+    'org.freefilesync.pkg.RealTimeSync',
+  ]
+end
