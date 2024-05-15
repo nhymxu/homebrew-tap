fea3da73b77d02a276518727998ec9a1a7352141
nhymxu <contact@dungnt.net>
2024-05-15 09:31:52 +0700

    add 3uTools

---
+++ Casks/3utools.rb
@@ -0,0 +1,15 @@
+cask "3utools" do
+  version "3.03.006"
+  sha256 :no_check # required as upstream package is updated in-place
+
+  url "https://dl.3u.com/update/mac/dl/3uTools_v#{version}.dmg"
+  name "3uTools"
+  desc "Useful Apple Mobile Device Management Tool"
+  homepage "https://www.3u.com/"
+
+  pkg "3utools_v#{version}.pkg"
+
+  uninstall pkgutil: [
+    'cn.3utools.mac',
+  ]
+end
