34e912792875985efe1422535e671f0703904393
Dung Nguyen <contact@dungnt.net>
2024-07-25 22:16:30 +0700

    freefilesync: update v13.7

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,5 +1,5 @@
 cask 'freefilesync' do
-  version '13.6'
+  version '13.7'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip"
