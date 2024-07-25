b7c355e75f59fd49a61d82b6c8e7d90b01985f43
Dung Nguyen <contact@dungnt.net>
2024-07-25 22:17:31 +0700

    usql: update v0.19.3

--- Casks/usql.rb
+++ Casks/usql.rb
@@ -1,9 +1,8 @@
 cask "usql" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.17.5"
-  sha256  arm:   "08d1f56a393eeca6c34919e170beb6abe184ac07ec9ec37c344bfa683e0a04c7",
-          intel: "1fabe95ea1b4d608579026643d29d12e05eee0e0c97a7e9ac57628f5889842cb"
+  version "0.19.3"
+  sha256 :no_check
 
   url "https://github.com/xo/usql/releases/download/v#{version}/usql-#{version}-darwin-#{arch}.tar.bz2"
   name "xo/usql"
