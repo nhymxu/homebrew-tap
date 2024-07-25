405fbe60e06ce081f264db290297f5bf7af4206e
Dung Nguyen <contact@dungnt.net>
2024-07-25 22:15:32 +0700

    defguard-client: update v0.4.0

--- Casks/defguard-client.rb
+++ Casks/defguard-client.rb
@@ -1,9 +1,9 @@
 cask "defguard-client" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.3.0"
-  sha256  arm:   "63bb263ce2c9c6b14a808cabf48335f13a7ca1300488e724a646f151ae751ea7",
-          intel: "98ced5d2c332e4626376d24bdd8ffb4188d6b8ef86a48f4e5c9069831ca783da"
+  version "0.4.0"
+  sha256  arm:   "3a195427681dc9eac592d2d924d68ade7c9b27205d99fa5fcff127bcccf2a8a6",
+          intel: "db13d31530075396d2ac799d0d3688ea524c919a7b65332feb695b5bcdf38c37"
 
   url "https://github.com/DefGuard/client/releases/download/v#{version}/defguard-#{arch}-apple-darwin-#{version}.pkg",
       verified: "github.com/DefGuard/client/"
