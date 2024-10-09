1d0b92d5aac71b4441febe2c930bb46a750b2cdb
Dung Nguyen <contact@dungnt.net>
2024-10-09 16:42:55 +0700

    battery-toolkit: fix url

--- Casks/battery-toolkit.rb
+++ Casks/battery-toolkit.rb
@@ -2,7 +2,7 @@ cask "battery-toolkit" do
   version "1.5"
   sha256 :no_check
 
-  url "https://github.com/mhaeuser/Battery-Toolkit/releases/download/#{version}/Battery-Toolkit-#{version}zip"
+  url "https://github.com/mhaeuser/Battery-Toolkit/releases/download/#{version}/Battery-Toolkit-#{version}.zip"
   name "Battery-Toolkit"
   desc "Control the platform power state of your Apple Silicon Mac."
   homepage "https://github.com/mhaeuser/Battery-Toolkit"
