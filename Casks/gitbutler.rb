69515812d286de6018476e462dc8db4c6bef42a2
Dung Nguyen <535617+nhymxu@users.noreply.github.com>
2025-04-21 20:29:59 +0700

    gitbutler 0.14.18

--- Casks/gitbutler.rb
+++ Casks/gitbutler.rb
@@ -2,7 +2,7 @@ cask "gitbutler" do
     arch arm: "aarch64", intel: "x86_64"
     arch2 arm: "aarch64", intel: "x64"
 
-    version "0.14.13-1764"
+    version "0.14.18-1859"
     sha256 :no_check # required as upstream package is updated in-place
 
     # url "https://releases.gitbutler.com/releases/release/#{version}/macos/#{arch}/GitButler_#{version.sub(/\-\d+/, "")}_#{arch2}.dmg"
