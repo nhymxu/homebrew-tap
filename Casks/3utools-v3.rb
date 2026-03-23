9b52bb90b0bdedf4036e4ca0e5e72ed510768ed9
Dung Nguyen <535617+nhymxu@users.noreply.github.com>
2026-03-23 12:39:46 +0700

    3utools: bump to v9, migrate old version to legacy cask

---
+++ Casks/3utools-v3.rb
@@ -0,0 +1,17 @@
+cask "3utools-v3" do
+  version "3.18.008"
+  sha256 :no_check # required as upstream package is updated in-place
+
+  # url "https://url2.3u.com/yiiuaqaa"
+  url "https://dl.3u.com/update/mac/dl/3uTools_v#{version}.dmg"
+  
+  name "3uTools v3 (legacy)"
+  desc "Useful Apple Mobile Device Management Tool"
+  homepage "https://www.3u.com/"
+
+  pkg "3utools_v#{version}.pkg"
+
+  uninstall pkgutil: [
+    'cn.3utools.mac',
+  ]
+end
--- Casks/3utools.rb
+++ Casks/3utools.rb
@@ -1,17 +1,18 @@
 cask "3utools" do
-  version "3.18.008"
+  arch arm: "arm64", intel: "x64"
+
+  version "9.01.010"
   sha256 :no_check # required as upstream package is updated in-place
 
-  # url "https://url2.3u.com/yiiuaqaa"
-  url "https://dl.3u.com/update/mac/dl/3uTools_v#{version}.dmg"
+  url "https://dl.3u.com/update/v900/macos/#{arch}/3uTools_v#{version}_#{arch}.dmg"
   
   name "3uTools"
-  desc "Useful Apple Mobile Device Management Tool"
-  homepage "https://www.3u.com/"
+  desc "Your iPhone Management Expert. All-in-one solution for your iPhone: Device Verification, Flashing, Backup, Optimization, and App downloads"
+  homepage "https://www.3u.com/productsWin"
 
-  pkg "3utools_v#{version}.pkg"
+  pkg "3utools_arm64.pkg"
 
   uninstall pkgutil: [
-    'cn.3utools.mac',
+    'com.3utools.mac',
   ]
 end
