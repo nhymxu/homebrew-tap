456c254bc8e63a06a095c26660dae8fcf459ecb7
Dung Nguyen <535617+nhymxu@users.noreply.github.com>
2026-03-31 13:42:48 +0700

    fix: working version tested

--- Formula/cliproxyapi-plus.rb
+++ Formula/cliproxyapi-plus.rb
@@ -2,46 +2,44 @@ class CliproxyapiPlus < Formula
   desc "The Plus version of CLIProxyAPI. Wrap Gemini CLI, Codex, Claude Code, Qwen Code as an API service"
   homepage "https://github.com/router-for-me/CLIProxyAPIPlus"
   version "6.9.5-0"
-  license "MIT"
-
-  on_macos do
-    on_arm do
-      url "https://github.com/router-for-me/CLIProxyAPIPlus/releases/download/v#{version}/CLIProxyAPIPlus_#{version}_darwin_arm64.tar.gz"
-      sha256 :no_check
-    end
-
-    on_intel do
-      url "https://github.com/router-for-me/CLIProxyAPIPlus/releases/download/v#{version}/CLIProxyAPIPlus_#{version}_darwin_amd64.tar.gz"
-      sha256 :no_check
-    end
+  
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/router-for-me/CLIProxyAPIPlus/releases/download/v#{version}/CLIProxyAPIPlus_#{version}_darwin_arm64.tar.gz"
+    sha256 "2c5ba34c8d216198fa5b0c4c641bcc2dc4eba0b09ac3dbaeab759ab6941960f0"
   end
 
-  livecheck do
-    url :url
-    strategy :github_latest
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/router-for-me/CLIProxyAPIPlus/releases/download/v#{version}/CLIProxyAPIPlus_#{version}_darwin_amd64.tar.gz"
+    sha256 "baba18529cbc85a369aa273f31f7810ce965f755f881cd286c663da27a4a7ac8"
   end
 
   def install
     bin.install "cli-proxy-api-plus"
-    (etc/"cliproxyapi-plus.yaml").write (buildpath/"config.example.yaml").read unless (etc/"cliproxyapi-plus.yaml").exist?
+    etc.install "config.example.yaml" => "cliproxyapi-plus.yaml"
+	# rm bin/"cliproxyapi-plus"
+    (bin/"cliproxyapi-plus").write <<~SHELL
+      #!/bin/sh
+      exec '#{bin}/cli-proxy-api-plus' --config '#{etc}/cliproxyapi-plus.yaml' "$@"
+    SHELL
   end
 
   service do
-    run [opt_bin/"cli-proxy-api-plus", "--config", etc/"cliproxyapi-plus.yaml"]
+    # run [opt_bin/"cli-proxy-api-plus", "--config", etc/"cliproxyapi-plus.yaml"]
+    run [opt_bin/"cliproxyapi-plus"]
     keep_alive true
-    log_path var/"log/cliproxyapi-plus.log"
-    error_log_path var/"log/cliproxyapi-plus-error.log"
   end
 
+      # To start cliproxyapi-plus now and restart at login:
+      #   brew services start nhymxu/tap/cliproxyapi-plus
+      #
+      # To stop the service:
+      #   brew services stop nhymxu/tap/cliproxyapi-plus
   def caveats
     <<~EOS
       You can edit settings at #{etc}/cliproxyapi-plus.yaml.
-
-      To start cliproxyapi-plus now and restart at login:
-        brew services start nhymxu/tap/cliproxyapi-plus
-
-      To stop the service:
-        brew services stop nhymxu/tap/cliproxyapi-plus
+      
+      Or, if you don't want/need a background service you can just run:
+        /opt/homebrew/bin/cliproxyapi-plus
     EOS
   end
 
