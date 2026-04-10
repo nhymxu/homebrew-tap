fc76f2e88bcbd8e14cce79135b053e1089ac1aa5
nhymxu <535617+nhymxu@users.noreply.github.com>
2026-04-10 00:25:28 +0000

    chore: update cliproxyapi-plus to 6.9.19-0

--- Formula/cliproxyapi-plus.rb
+++ Formula/cliproxyapi-plus.rb
@@ -1,16 +1,16 @@
 class CliproxyapiPlus < Formula
   desc "The Plus version of CLIProxyAPI. Wrap Gemini CLI, Codex, Claude Code, Qwen Code as an API service"
   homepage "https://github.com/router-for-me/CLIProxyAPIPlus"
-  version "6.9.18-0"
+  version "6.9.19-0"
   
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/router-for-me/CLIProxyAPIPlus/releases/download/v#{version}/CLIProxyAPIPlus_#{version}_darwin_arm64.tar.gz"
-    sha256 "8e722383d5d1a521d682b94150bd81da8014e19e81e61b8048c3b1fe4ea555f7"
+    sha256 "420403d6c85cec460c6f3ad698e2487a611b23c86531ecc4c6036d3ffce16450"
   end
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/router-for-me/CLIProxyAPIPlus/releases/download/v#{version}/CLIProxyAPIPlus_#{version}_darwin_amd64.tar.gz"
-    sha256 "fcc0064af24ceb30ad027e8962ef3befe198814b1cdd34227aa7b679a8093ad5"
+    sha256 "51da8b562652c9e02d95278bf13495a48a9f7d1198da633db526ea6d2448565a"
   end
 
   def install
