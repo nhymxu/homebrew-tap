e875a58a967c06d1200d68e1dc6d0252441c9b49
Dung Nguyen <535617+nhymxu@users.noreply.github.com>
2026-06-08 23:16:18 +0700

    add itsypad

---
+++ Casks/itsypad.rb
@@ -0,0 +1,18 @@
+cask "itsypad" do
+  version "1.16.0"
+  sha256 :no_check
+
+  url "https://github.com/nickustinov/itsypad-macos/releases/download/v#{version}/Itsypad-#{version}.dmg"
+  name "Itsypad"
+  desc "A tiny, fast scratchpad and clipboard manager for Mac"
+  homepage "https://github.com/nickustinov/itsypad-macos"
+
+  depends_on macos: :sonoma
+
+  app "Itsypad.app"
+
+  zap trash: [
+    "~/Library/Application Support/Itsypad",
+    "~/Library/Preferences/com.nickustinov.itsypad.plist",
+  ]
+end
